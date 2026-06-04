8433d9ae76e381f8edd6ee941c94ca78b6ea9934
Omkar Patil <ompatil5132@gmail.com>
2026-06-04 20:19:16 +0530

    routes created

--- server/routes/ticket.route.js
+++ server/routes/ticket.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {createTicket,getTickets,getTicketById,updateTicket,} = require("../controllers/ticket.controller");
+const {createTicket,getTickets,getTicketById,updateTicket} = require("../controllers/ticket.controller");
 
 const router = express.Router();
 
